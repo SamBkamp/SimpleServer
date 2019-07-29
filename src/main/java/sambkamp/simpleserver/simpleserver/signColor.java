ce39b005c5470750a0eca941cd514e6cb0dad239
sam bonnekamp <samuel@bonnekamp.net>
2019-07-29 12:15:18 +0800

    changed sign place logic so plugin now gets data from event block as opposed to a casted block. Fixes bug that didnt change color of sign text when a trade sign was placed.

--- src/main/java/sambkamp/simpleserver/simpleserver/signColor.java
+++ src/main/java/sambkamp/simpleserver/simpleserver/signColor.java
@@ -2,7 +2,6 @@ package sambkamp.simpleserver.simpleserver;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
@@ -10,17 +9,11 @@ import org.bukkit.event.block.SignChangeEvent;
 public class signColor implements Listener {
     @EventHandler
     public void signChange(SignChangeEvent e){
-        if (e.getBlock().getType().equals(Material.OAK_WALL_SIGN) || e.getBlock().getType().equals(Material.OAK_SIGN)){
-            Sign sign = (Sign) e.getBlock().getState();
-            String amp[] = sign.getLine(0).split(" - ");
-            e.getPlayer().sendMessage(sign.getLine(0));
-            if (!amp[0].equals("[T]")){
-                return;
-            }
-            sign.setLine(0, ChatColor.LIGHT_PURPLE + sign.getLine(0));
-            sign.update();
+        if (e.getBlock().getType() == Material.OAK_WALL_SIGN || e.getBlock().getType() == Material.OAK_SIGN){
+            String amp[] = e.getLine(0).split(" - "); //sign parsing stuff
+            if (!amp[0].equals("[T]")) return;
+            e.setLine(0, ChatColor.LIGHT_PURPLE + e.getLine(0));
             e.getPlayer().sendMessage(ChatColor.AQUA + "Shop set!");
-            return;
         }
     }
 }
